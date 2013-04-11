c2f26a57fd75372322479679fef147a3b0209c69
WolfieZero <neil@wolfiezero.com>
2013-04-11 13:34:59 +0100

    Added file for WordPress

---
+++ wp-geotools.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Geotools
+Description: WordPress pluging to use Geotools
+Version: 1.0
+Author: Antoine Corcy (Geotools) and Neil Sweeney (WP)
+Plugin URI: http://geotools-php.org/
+License: Copyright (c) 2013 Antoine Corcy <contact@sbin.dk>
+*/
+
+require 'src/autoload.php';
